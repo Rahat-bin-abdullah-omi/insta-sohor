25095a3dbab32558acbe8a214f67175f746d1b6e
Rahat Bin Abdullah Omi <rahatomi123@gmail.com>
2022-03-11 22:07:55 +0600

    minor bug fixed

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ const getReportedPosts = () => {
 };
 
 const isLiked = (id) => {
-    return likedPostsId ? .length && !!likedPostsId.includes(id);
+    return likedPostsId.length && !!likedPostsId.includes(id);
 };
 
 const addToLiked = (id) => {
@@ -67,10 +67,12 @@ const createPost = (post) => {
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
+
                 <button class="post__more-options">
                   <i class="fa-solid fa-ellipsis"></i>
                 </button>
               </div>
+
               <div class="post__content">
                 <div class="post__medias">
                   <img
@@ -80,6 +82,7 @@ const createPost = (post) => {
                   />
                 </div>
               </div>
+
               <div class="post__footer">
                 <div class="post__buttons">
                   <button class="post__button" onclick="addToLiked(${post.id})">
@@ -90,23 +93,30 @@ const createPost = (post) => {
                     <i class="fa-solid fa-comment"></i>
                   </button>
                   
+
                   <div class="post__indicators"></div>
+
                   <button class="post__button post__button--align-right" onclick="reportPost(${post.id
     })">
                     <i class="fa-solid fa-ban"></i>
                   </button>
                 </div>
+
                 <div class="post__content">${displayContent(post.description)}</div>
+
                 <div class="post__infos">
                   <div class="post__likes">
                     <a href="#" class="post__likes-avatar">
                       <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="User Picture" />
                     </a>
+
                     <span>Liked by
                       <a class="post__name--underline" href="#">user123</a> and
                       <a href="#">73 others</a></span>
                   </div>
+
                   <hr/>
+
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
